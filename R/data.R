59c66d0f71f99b04189f50e2775ad7c283c4f950
neitmant <thomas.neitmann@roche.com>
2021-11-05 15:44:55 +0100

    Create package

---
+++ R/data.R
@@ -0,0 +1,96 @@
+#' Adverse Events Dataset
+#'
+#' A SDTM AE dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt}
+"ae"
+
+#' Concomitant Medication Dataset
+#'
+#' A SDTM CM dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/cm.xpt}
+"cm"
+
+#' Demography Dataset
+#'
+#' A SDTM DM dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt}
+"dm"
+
+#' Disposition Dataset
+#'
+#' A SDTM DS dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ds.xpt}
+"ds"
+
+#' Electrocardiogram Dataset
+#'
+#' An example of standard SDTM EG dataset to be used in deriving ADEG dataset
+#'
+"eg"
+
+#' Exposure Dataset
+#'
+#' A SDTM EX dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt}
+"ex"
+
+#' Laboratory Measurements Dataset
+#'
+#' A SDTM LB dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt}
+"lb"
+
+#' Medical History Dataset
+#'
+#' A SDTM MH dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/mh.xpt}
+"mh"
+
+#' Questionnaire Dataset
+#'
+#' A SDTM QS dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/qs.xpt}
+"qs"
+
+#' Supplemental Adverse Events Dataset
+#'
+#' A SDTM SUPPAE dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppae.xpt}
+"suppae"
+
+#' Supplemental Disposition Dataset
+#'
+#' A SDTM SUPPDS dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppds.xpt}
+"suppds"
+
+#' Supplemental Demography Dataset
+#'
+#' A SDTM SUPPDM dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppdm.xpt}
+"suppdm"
+
+#' Trial Design Dataset
+#'
+#' A SDTM TS dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ts.xpt}
+"ts"
+
+#' Vital Signs Dataset
+#'
+#' A SDTM VS dataset from the CDISC pilot project
+#'
+#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/vs.xpt}
+"vs"
