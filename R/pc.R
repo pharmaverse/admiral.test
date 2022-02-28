53469ed480d00db8fbf5ef8821582cf760e8e214
neitmant <thomas.neitmann@roche.com>
2022-02-28 16:48:52 +0100

    Apply `styler::tidyverse_style()`

--- R/pc.R
+++ R/pc.R
@@ -1,15 +1,15 @@
-library(haven) #Load xpt
+library(haven) # Load xpt
 library(plyr)
-library(dplyr) #apply distincts
+library(dplyr) # apply distincts
 library(lubridate)
 library(ggplot2)
 
 data("ex")
 data("dm")
 
-#Remove screen failures, they will not make it to drug infusion
+# Remove screen failures, they will not make it to drug infusion
 dm1 <- dm %>%
-      filter(ARMCD != "Scrnfail")
+  filter(ARMCD != "Scrnfail")
 
 # use subjects in both datasets (ex,dm1) to create pc
 dmex <- merge(dm1[, c(1, 3)], ex, by = c("STUDYID", "USUBJID"))
@@ -17,7 +17,7 @@ dmex <- merge(dm1[, c(1, 3)], ex, by = c("STUDYID", "USUBJID"))
 # I'm going to calculate only PC on Baseline,
 # so going to filter onlyvisit = BASELINE
 dmex1 <- dmex %>%
-        filter(VISIT == "BASELINE")
+  filter(VISIT == "BASELINE")
 
 # We can check on figure 2 that the patch behaves like
 # 2 phases (absorption up to the plateau, and elimination)
@@ -28,8 +28,7 @@ t <- c(-0.5, 0.08, 0.5, 1, 1.5, 2, 4, 6, 8, 12, 16, 24, 36, 48)
 # add Calculate some noise
 nrows <- dim(dmex1)[1]
 noise <- runif(n = nrows, min = -5, max = 5)
-dmex1$k0 <- 100 + noise;
-
+dmex1$k0 <- 100 + noise
 # K is the elimination rate (absorption significantly larger)
 # add Calculate some noise
 noise <- runif(n = nrows, min = -0.05, max = 0.05)
@@ -37,30 +36,30 @@ dmex1$K <- 0.7 + noise
 dmex1$V <- dmex1$EXDOSE / (dmex1$K)
 
 # init dataset
-PC <- NULL;
+PC <- NULL
 for (t0 in t) {
   dmex1$t <- t0
-  #dmex1$Conc = ifelse(dmex1$EXDOSE>0,(k0 * (1 - exp(-K*t0)))/(V*K),0)
-  #Zero order
-  #T = t0;
-  #if (t0>10) {T = 10}
-  #dmex1$Conc = ifelse(dmex1$EXDOSE>0,
+  # dmex1$Conc = ifelse(dmex1$EXDOSE>0,(k0 * (1 - exp(-K*t0)))/(V*K),0)
+  # Zero order
+  # T = t0;
+  # if (t0>10) {T = 10}
+  # dmex1$Conc = ifelse(dmex1$EXDOSE>0,
   #                    (k0 * (exp(K*T) - 1) * exp(-K*t0))/(V*K),0)
 
-  #First order
+  # First order
   if (t0 > 8) {
     dmex1$Conc <- ifelse(dmex1$EXDOSE > 0,
-                        dmex1$k0 / (dmex1$V * dmex1$K) * exp(-(dmex1$K*(t0 - 8)) / 2.303), 0)
+      dmex1$k0 / (dmex1$V * dmex1$K) * exp(-(dmex1$K * (t0 - 8)) / 2.303), 0
+    )
+  } else {
+    dmex1$Conc <- ifelse(dmex1$EXDOSE > 0,
+      (dmex1$k0 * (1 - exp(-dmex1$K * t0))) / (dmex1$V * dmex1$K), 0
+    )
   }
-  else {
-    dmex1$Conc <- ifelse(dmex1$EXDOSE>0,
-                      (dmex1$k0 * (1 - exp(-dmex1$K*t0))) / (dmex1$V*dmex1$K) ,0)
-  }
-  if (t0 == -0.5) { #If first timepoint
+  if (t0 == -0.5) { # If first timepoint
 
     PC <- dmex1
-  }
-  else { #Otherwise
+  } else { # Otherwise
     PC <- rbind(PC, dmex1)
   }
 }
@@ -68,7 +67,7 @@ for (t0 in t) {
 # Constant var
 PC$DOMAIN <- "PC"
 PC$PCTESTCD <- "XAN"
-PC$PCTEST   <- "XANOMELINE"
+PC$PCTEST <- "XANOMELINE"
 PC$PCORRESU <- "ug/ml"
 PC$PCSTRESU <- "ug/ml"
 PC$PCNAM <- "Imaginary Labs"
@@ -77,7 +76,9 @@ PC$PCLLOQ <- 0.01
 PC$PCTPTNUM <- PC$t
 
 # PCSEQ;
-PC <- PC %>% group_by(STUDYID, USUBJID) %>% dplyr::mutate(PCSEQ = row_number())
+PC <- PC %>%
+  group_by(STUDYID, USUBJID) %>%
+  dplyr::mutate(PCSEQ = row_number())
 
 # Concentration related
 # Remove neg values due to pre-dose negative time
@@ -99,28 +100,32 @@ PC$PCSTRESC <- PC$Conc
 
 # Probably can be done throw factors
 PC$PCTPT <- ifelse(PC$PCTPTNUM == -0.5, "Pre-dose",
-              ifelse(PC$PCTPTNUM == 0.08, "5 Min Post-dose",
-                ifelse(PC$PCTPTNUM == 0.5, "30 Min Post-dose", paste0(PC$PCTPTNUM, "h Post-dose")
-                       )
-                     )
-                  )
+  ifelse(PC$PCTPTNUM == 0.08, "5 Min Post-dose",
+    ifelse(PC$PCTPTNUM == 0.5, "30 Min Post-dose", paste0(PC$PCTPTNUM, "h Post-dose"))
+  )
+)
 
 PC$PCDTC <- as.Date(PC$EXSTDTC) + minutes(round(PC$t * 60))
 PC$PCDY <- ifelse(PC$t == -0.5, -1,
-                 ifelse(PC$t == 48, 3,
-                        ifelse(PC$t >= 24, 2, 1)
-                        )
-                 )
+  ifelse(PC$t == 48, 3,
+    ifelse(PC$t >= 24, 2, 1)
+  )
+)
 
-PC <- subset(PC, select = c("STUDYID", "DOMAIN", "USUBJID", "PCSEQ", "PCTESTCD", "PCTEST",
-            "PCORRES", "PCORRESU", "PCSTRESC", "PCSTRESN", "PCSTRESU",
-            "PCNAM", "PCSPEC", "PCLLOQ", "VISIT", "VISITNUM", "PCDTC", "PCDY", "PCTPT", "PCTPTNUM"))
-pc <- PC %>% ungroup() %>% arrange(STUDYID, USUBJID, PCSEQ)
+PC <- subset(PC, select = c(
+  "STUDYID", "DOMAIN", "USUBJID", "PCSEQ", "PCTESTCD", "PCTEST",
+  "PCORRES", "PCORRESU", "PCSTRESC", "PCSTRESN", "PCSTRESU",
+  "PCNAM", "PCSPEC", "PCLLOQ", "VISIT", "VISITNUM", "PCDTC", "PCDY", "PCTPT", "PCTPTNUM"
+))
+pc <- PC %>%
+  ungroup() %>%
+  arrange(STUDYID, USUBJID, PCSEQ)
 
 
 # Some test to look the overall figure
-plot <- ggplot(pc, aes(x = PCTPTNUM, y = PCSTRESN, group = USUBJID)) + geom_line() +
-                                                   geom_point()
+plot <- ggplot(pc, aes(x = PCTPTNUM, y = PCSTRESN, group = USUBJID)) +
+  geom_line() +
+  geom_point()
 
 # ---- Save output ----
 
