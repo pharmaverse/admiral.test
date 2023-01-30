d7263984a18553e60ee9a11f8e84a12951f1cf28
Gordon Miller <gordon.miller@roche.com>
2023-01-30 14:39:27 +0000

    99 Fix typo in update_ae.R

--- dev/update_ae.R
+++ dev/update_ae.R
@@ -4,11 +4,6 @@ library(dplyr)
 library(admiral.test)
 library(admiral)
 
-data("admiral_ae")
-
-raw_ae <- admiral_ae
-
-
 data("raw_ae")
 ae <- convert_blanks_to_na(raw_ae)
 
@@ -24,7 +19,8 @@ ae$AELAT <- if_else(ae$AESOC == "EYE DISORDERS",
                 NA_character_
                 )
 admiral_ae <- ae
-attr(admiral_mh, "label") <- "Adverse Events"
+
+attr(admiral_ae, "label") <- "Adverse Events"
 
 # Save dataset ----
-saveRDS(admiral_ae, "data/admiral_ae.rda", compress = "bzip2")
+save(admiral_ae, "data/admiral_ae.rda", compress = "bzip2")
