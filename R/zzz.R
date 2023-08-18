e3e2c2f8f49ef85adadb3a0ba84d316a28b85f7b
Zelos Zhu <zdz2101@github.com>
2023-08-18 18:07:18 +0000

    chore: #145 cleanup messaging

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    pkgname, " v",
-    packageVersion(pkgname),
-    " will be the final version. ",
+    "admiral.test v0.7.0 ",
+    "will be the final version. ",
     "At the end of 2023, ",
     "the package will be archived in favor of pharmaversesdtm."
   )
